de8b22f9ad5147a4f648fd6de9c12f2433cff9ea
A1rPun <thombakker@gmail.com>
2016-04-15 03:20:52 +0200

    :fast_forward: Implemented simple methods

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 ﻿#jinq.js
 JavaScript Integrated query
+
+
+	jinq([5,6,3,1,2,9,0,4,7,8])
+		.where(function(obj) {
+			return obj > 1;
+		})
+		.select(function(obj){
+			return {
+				original: obj,
+				multiplied: obj * 2,
+				isEven: obj % 2 === 0
+			};
+		})
+		.groupBy(function(obj){
+			return obj.isOdd;
+		})
+		.toArray();
--- jinq.js
+++ jinq.js
@@ -8,8 +8,79 @@
     }
 }('jinq', function () {
 
-    function jinq() {
-
+    function enumerable(arr) {
+        this._data = arr;
     }
-    return jinq;
+
+    enumerable.prototype = {
+        any: function () {
+            return !!this.first()
+        },
+        count: function () {
+            return this.toArray().length;
+        },
+        groupBy: function (fn) {
+            var result = [];
+            var prop = 'key';
+            for (var i = 0, l = this._data.length; i < l; i++) {
+                var obj = this._data[i];
+                var group = fn.call(this._data, obj, i);
+                var index = getIndexOf(result, prop, group);
+
+                if (index === -1)
+                    result.push({
+                        key: group,
+                        value: [obj]
+                    });
+                else
+                    result[index].value.push(obj);
+            }
+            this._data = result;
+            return this;
+        },
+        first: function () {
+            var result = this.toArray();
+            var length = result.length;
+            return length ? result[0] : null;
+        },
+        last: function () {
+            var result = this.toArray();
+            var length = result.length;
+            return length ? result[result.length - 1] : null;
+        },
+        orderBy: function () {
+            var newArr = this._data.slice();
+            this._data = newArr.sort.apply(newArr, arguments);
+            return this;
+        },
+        select: function (fn) {
+            this._data = this._data.map.apply(this._data, arguments);
+            return this;
+        },
+        //thenBy: function () { },
+        toArray: function () {
+            return this._data;
+        },
+        where: function (fn) {
+            var result = [];
+            for (var i = 0, l = this._data.length; i < l; i++) {
+                var obj = this._data[i];
+                if (fn.call(this._data, obj, i))
+                    result.push(obj);
+            }
+            this._data = result;
+            return this;
+        }
+    };
+
+    function getIndexOf(arr, prop, val) {
+        for (var i = 0, l = arr.length; i < l; i++)
+            if (arr[i][prop] === val)
+                return i;
+        return -1;
+    }
+
+    return function jinq(arr) {
+        return new enumerable(arr);
+    };
 }));
