97af1271bea457a9561323065d804e06aedb3a05
A1rPun <thombakker@gmail.com>
2022-01-04 19:43:54 +0100

    :beetle: Always use sortFn for orderBy

--- src/orderBy.js
+++ src/orderBy.js
@@ -1,3 +1,6 @@
-export function* orderBy(generator, sortFn = undefined) {
+export function* orderBy(
+  generator,
+  sortFn = (a, b) => (a < b ? -1 : b < a ? 1 : 0)
+) {
   yield* [...generator].sort(sortFn);
 }
