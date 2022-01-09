d8ecc2d42cc3d927d42b08e3b0375e978067d444
A1rPun <thombakker@gmail.com>
2022-01-09 18:05:16 +0100

    :zap: Improve except & intersect

--- src/except.js
+++ src/except.js
@@ -1,9 +1,8 @@
 import { toDictionary } from './toDictionary.js';
 
 export function* except(generator, list, groupBy = (v) => v) {
-  const genLookup = toDictionary(generator, groupBy);
   const listLookup = toDictionary(list, groupBy);
 
-  for (const prop in genLookup)
-    if (listLookup[prop] === undefined) yield genLookup[prop];
+  for (const value of generator)
+    if (listLookup[groupBy(value)] === undefined) yield value;
 }
