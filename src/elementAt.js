803b9bf7252d9229306eea412c0c696cf820b734
A1rPun <thombakker@gmail.com>
2024-06-19 18:56:22 +0200

    :wrench: Look for another way to do array-first

--- src/elementAt.js
+++ src/elementAt.js
@@ -2,12 +2,6 @@ import { ArgumentOutOfRangeException } from './errors.js';
 
 export function elementAt(source, atIndex) {
   if (atIndex < 0) throw new ArgumentOutOfRangeException();
-
-  if (Array.isArray(source)) {
-    if (source.length <= atIndex) throw new ArgumentOutOfRangeException();
-    return source.at(atIndex);
-  }
-
   let index = 0;
 
   for (const element of source)
