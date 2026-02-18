4a66f44d1ed294fdc4d43eb874a508a72b083fec
A1rPun <thombakker@gmail.com>
2026-02-18 21:20:06 +0800

    :building_construction: Move static function defs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import * as functions from './src/index.js';
 import { Enumerable } from './src/Enumerable.js';
 
+const staticFunctions = [
+  'empty',
+  'infiniteSequence',
+  'iterate',
+  'range',
+  'repeat',
+  'sequence',
+];
+
 for (const [name, fn] of Object.entries(functions)) {
-  if (Enumerable.prototype[name]) continue;
-
-  const isGenerator = fn.constructor.name === 'GeneratorFunction';
-
-  if (isGenerator) {
+  if (staticFunctions.includes(name)) {
+    Enumerable[name] = function (...args) {
+      return new Enumerable(fn(...args));
+    };
+  } else if (fn.constructor.name === 'GeneratorFunction') {
     Enumerable.prototype[name] = function (...args) {
       return new Enumerable(fn(this.sequence, ...args));
     };
--- src/Enumerable.js
+++ src/Enumerable.js
@@ -1,4 +1,3 @@
-import { empty, infiniteSequence, iterate, range, repeat, sequence } from './index.js';
 import { ReplaySubject } from './ReplaySubject.js';
 
 export class Enumerable {
@@ -14,31 +13,7 @@ export class Enumerable {
     return this.sequence.length;
   }
 
-  static empty() {
-    return new Enumerable(empty());
-  }
-
   static from(iterator) {
     return new Enumerable(iterator);
-  }
-
-  static infiniteSequence(start, step) {
-    return new Enumerable(infiniteSequence(start, step));
-  }
-
-  static iterate(seed, selector) {
-    return new Enumerable(iterate(seed, selector));
-  }
-
-  static range(start, count) {
-    return new Enumerable(range(start, count));
-  }
-
-  static repeat(value, count) {
-    return new Enumerable(repeat(value, count));
-  }
-
-  static sequence(start, endInclusive, step) {
-    return new Enumerable(sequence(start, endInclusive, step));
   }
 }
